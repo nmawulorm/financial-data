d07b62034b24fee9888b36c39026bb6ad036c815
= <=>
2025-01-10 18:20:58 +0000

    Managed to be able to fit parameters (Beta0, Beta1, Beta2 and Lambda)

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import pandas as pd
 import pandas_datareader.data as web
-import matplotlib.pyplot as plt
 from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
 
 
 # Define the start and end dates for the data retrieval
@@ -41,3 +45,18 @@ plt.title('U.S. Treasury Yield Curve')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+def nelson_siegel(maturity, beta0, beta1, beta2, lambda_):
+    term1 = beta0
+    term2 = beta1 * (1 - np.exp(-lambda_ * maturity)) / (lambda_ * maturity)
+    term3 = beta2 * ((1 - np.exp(-lambda_ * maturity)) / (lambda_ * maturity) - np.exp(-lambda_ * maturity))
+    return term1 + term2 + term3
+
+maturities = [1/12, 3/12, 6/12, 1, 2, 3, 5, 7, 10, 20, 30]  # Convert months to years
+yields = treasury_data.iloc[-1].values  # Use the latest yield data for fitting
+
+# Initial guess for parameters
+initial_guess = [4, -1, 1, 0.5]  # Adjust these as needed
+params, _ = curve_fit(nelson_siegel, maturities, yields, p0=initial_guess)
+
+print("Fitted Parameters (Beta0, Beta1, Beta2, Lambda):", params)
